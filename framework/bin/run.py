89d88860446d5e154e3d298f52ea840e18248044
sumithdcosta <42089787+sumithdcosta@users.noreply.github.com>
2021-07-22 13:36:40 +0530

    Created using Colaboratory

---
+++ framework/bin/run.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled75.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyPOTFLsA02J0M7dzg1f0Orh",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/sumithdcosta/Python/blob/master/framework/bin/run.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "emhy2nvZPq48"
+      },
+      "source": [
+        ""
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
